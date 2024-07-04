be269ddab2b9a34837b23a1e3bd387002a14e5d2
Messias <luckasreyes945@gmail.com>
2024-07-04 18:56:27 -0400

    titulo responsivo moviles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
-                <motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-12 lg:text-5xl">Luckas Matías Reyes Gómez</motion.h1>
+                <motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl sm:pb-16 sm:text-6xl font-thin tracking-tight lg:mt-12 lg:text-5xl">Luckas Matías Reyes Gómez</motion.h1>
                 <motion.span variants={container(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Software Engineer</motion.span>
                 <motion.p variants={container(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
             </div>
