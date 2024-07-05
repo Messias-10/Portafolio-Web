f22d25c106ecc03e6e912f29d4bd5c17a698a143
Messias <luckasreyes945@gmail.com>
2024-07-04 23:18:13 -0400

    Configuracion tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,33 +9,48 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        "loop-scroll":{
-          from: {transform: "translateX(0)"},
-          to: {transform: "translateX(-100%)"},
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
         },
-      typing: {
-        "0%": {
-          width: "0%",
-          visibility: "hidden"
+        'text-slide-3': {
+          '0%, 26.66%': {
+            transform: 'translateY(0%)',
+          },
+          '33.33%, 60%': {
+            transform: 'translateY(-25%)',
+          },
+          '66.66%, 93.33%': {
+            transform: 'translateY(-50%)',
+          },
+          '100%': {
+            transform: 'translateY(-75%)',
+          },
+          typing: {
+            "0%": {
+              width: "0%",
+              visibility: "hidden"
+            },
+            "100%": {
+              width: "100%"
+            }
+          },
+          blink: {
+            "50%": {
+              borderColor: "transparent"
+            },
+            "100%": {
+              borderColor: "white"
+            }
+          }
         },
-        "100%": {
-          width: "100%"
-        }  
       },
-      blink: {
-        "50%": {
-          borderColor: "transparent"
-        },
-        "100%": {
-          borderColor: "white"
-        }  
-      }
-    },      
       animation: {
         "loop-scroll": "loop-scroll 50s linear infinite",
-        typing: "typing 2s steps(20), blink .7s infinite"
-      }
-  },
+        typing: "typing 2s steps(20), blink .7s infinite",
+        'text-slide-3': 'text-slide-3 8s cubic-bezier(0.83, 0, 0.17, 1) infinite'
+      },
+    },
   },
   plugins: [],
-}
+};
